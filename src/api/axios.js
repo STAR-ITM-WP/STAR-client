7c8ef44c53e9a38e740262548106f68cb7aa8bbe
Park Shinhyung <23102009@seoultech.ac.kr>
2025-06-18 08:27:53 +0900

    feat

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://star-isih.onrender.com', // 변경 시 여기만 수정
+  baseURL: 'https://star-isih.onrender.com', 
   withCredentials: true
 });
 
